d1ab89abd409c77e309c289728c06039328fe7dd
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-16 20:53:28 -0400

    Queries for exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name:"
+inputted_store = $stdin.gets.chomp
+new_store = Store.create(name: "#{inputted_store}")
+
+if new_store.save
+  puts "New store saved successfully!"
+else
+  puts "New store couldn't be saved. Errors:"
+  new_store.errors.each do |error|
+    puts error.full_message
+  end
+end
